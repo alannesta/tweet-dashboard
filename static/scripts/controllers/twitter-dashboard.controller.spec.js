d6859e0c30dc3fad7cccbe81a7a2244f18f812ac
Alan Cao <alannesta@sina.com>
2015-08-24 16:11:15 -0400

    clean up

--- static/scripts/controllers/twitter-dashboard.controller.spec.js
+++ static/scripts/controllers/twitter-dashboard.controller.spec.js
@@ -5,23 +5,28 @@ describe('Dashboard Controller Test', function () {
     var $controller;
     var $rootScope;
     var $q;
-    var pController;
+    var controller;
     var scope;
+    var Tweets;
 
     beforeEach(angular.mock.module('twitter-dashboard'));
 
-    beforeEach(inject(function (_$controller_, _$rootScope_, _$q_) {
+    beforeEach(inject(function (_$controller_, _$rootScope_, _$q_, _Tweets_) {
         $rootScope = _$rootScope_;
         $q = _$q_;
         $controller  = _$controller_;
         scope = $rootScope.$new();
+        Tweets = _Tweets_;
+        spyOn(Tweets, "get").and.returnValue([{id: 1}, {id: 2}]);
 
-        pController = $controller('DashboardController', {
-            $scope: scope
-        });
     }));
 
     it('should set up the tests', function(){
         console.log('tests running');
+        controller = $controller('DashboardController', {
+            $scope: scope,
+            Tweets: Tweets
+        });
+        expect(Tweets.get).toHaveBeenCalled();
     });
 });
