21d83f68e0e1a28dca0e062a43029f31037f3d18
Alan Cao <alannesta@sina.com>
2015-08-24 11:41:42 -0400

    column ordering

--- static/scripts/controllers/sidenav.controller.js
+++ static/scripts/controllers/sidenav.controller.js
@@ -24,14 +24,60 @@ angular
         //    },
         //    date: 'undefined'
         //};
-
         $scope.config = layoutConfig.getConfig();
         console.log($scope.config);
 
-        $scope.save = function() {
-            layoutConfig.saveConfig($scope.config);
-            $scope.$emit('refresh');
+        $scope.order = {
+            AppDirect: $scope.config.order.indexOf('AppDirect') + 1,
+            laughingsquid: $scope.config.order.indexOf('laughingsquid') + 1,
+            techcrunch: $scope.config.order.indexOf('techcrunch') + 1
         };
-        //console.log($scope.layoutConfig);
+
+        $scope.save = function() {
+            if(validateColumnOrder()){
+                layoutConfig.saveConfig($scope.config);
+                $scope.$emit('refresh');
+            }
+        };
+
+        function validateColumnOrder() {
+            if(validateDups($scope.order)) {
+                $scope.config.order = [];
+                for (var i = 1; i < 4; i++) {
+                    $scope.config.order.push(getKeyByValue($scope.order, i));
+                }
+                return true;
+            } else {
+                alert('please set the correct column order, no duplicates...');
+                return false;
+            }
+        }
+
+        function validateDups(obj) {
+            console.log(obj);
+            var values = [];
+            var dups = [];
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                   values.push(obj[prop]);
+                }
+            }
+
+            values.forEach(function(val){
+                if (dups.indexOf(val) < 0) {
+                    dups.push(val);
+                }
+            });
+            return values.length === dups.length;
+        }
+
+        function getKeyByValue(obj, value) {
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    if (obj[prop] === value)
+                        return prop;
+                }
+            }
+        }
     }]);
 
